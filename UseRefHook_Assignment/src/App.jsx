14f11b3e867ff5033501306dc0b4c68150e78376
Arpan Jain <arpanjain231995@gmail.com>
2024-05-27 18:49:44 +0530

    useRef Hook assignment complete

--- UseRefHook_Assignment/src/App.jsx
+++ UseRefHook_Assignment/src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value2, setValue2] = useState();
+  // problem statement 1.
+  const inputRef = useRef()
+  // problem statement 2.
+  const inputRef2 = useRef(null)
+  // problem statement 3.
+  const changeRef = useRef()
+
+  useEffect(() => {
+    // problem statement 1.
+    inputRef.current.focus()
+  }, []);
+
+  // problem statement 2.
+  const handleChange = () => {
+    const inputValue = inputRef2.current.value;
+    console.log('Current Value:', inputValue);
+    setValue2(inputValue)
+  };
+
+  // problem statement 3.
+  const changeColor = () => {
+    changeRef.current.style.backgroundColor = 'blue';
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Exploring useRef Hook in React</h1>
+        <p>Problem statement</p>
+        <p>1. Creating a Focusable Input Field</p>
+        <div>
+          <input type="text" ref={inputRef} />
+        </div>
+        <br />
+        <p>2. Managing Uncontrolled Components</p>
+        <div>
+          <input type="text" ref={inputRef2} onChange={handleChange}/>
+          <p>{value2}</p>
+        </div>
+        <br />
+        <p>3. Interacting with DOM Elements</p>
+        <div>
+          <div style={{width: "100px", height: "100px", backgroundColor: "red"}} ref={changeRef}></div>
+          <button onClick={changeColor}>Click to Change Color</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
